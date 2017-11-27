e8cec5b280d66a5c769341f1c2f61f7f12f8d71c
Johnny Shrewd <ionutghisoi95@gmail.com>
2017-11-27 13:53:26 +0000

    maybe

--- tests/features/step_definitions/test.spec.js
+++ tests/features/step_definitions/test.spec.js
@@ -13,7 +13,7 @@ export default function () {
     }, true);
   });
   this.Given(/^I check google$/, () => {
-    browser.url('http://localhost');
+    browser.url('http://johnny-shrewd-test.com');
     browser.pause(3000);
     console.log(browser.getSrc());
     browser.waitUntil(function () {
