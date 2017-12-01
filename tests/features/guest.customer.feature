d6fe8eb2b9350077d9e3815284a67b895a7252fa
Johnny Shrewd <ionutghisoi95@gmail.com>
2017-12-01 14:13:57 +0000

    Nice test

--- tests/features/guest.customer.feature
+++ tests/features/guest.customer.feature
@@ -8,35 +8,9 @@ Scenario: I should be able to complete a stage1
       Given I disable the url secret key encryption
       Given I go to the backend of Checkout's plugin
       Given I set the sandbox keys
-      Given I set the integration type to embedded
-      Given I set the payment option title
-      Given I set the payment mode to cards
-      Given I set the theme color
-      Given I set the button label
-      Given I enable Vault
-      Given I set Vault title
       Given I enable 3D Secure
-      Given I enable autocapture
       Given I save the backend settings
       Then I complete the order flow as a unregistered customer until the payment stage
       Then I choose Checkout as a payment option
-
-
-Scenario: I should be able to complete a stage1
-      Given I create an account
-      Then I logout from the registered customer account
-      Given I go to the backend of Checkout's plugin
-      Given I set the integration type to embedded
-      Given I set the payment option title
-      Given I set the payment mode to cards
-      Given I set the theme color
-      Given I set the button label
-      Given I enable Vault
-      Given I set Vault title
-      Given I enable 3D Secure
-      Given I enable autocapture
-      Given I save the backend settings
-      Given I login the registered customer account
-      Then I complete the order flow as a registered customer until the payment stage
-      Then I choose Checkout as a payment option
-      Then I logout from the registered customer account
+      Then I complete Checkout Frames with a visa card
+      Then I submit the order for the frames integration
--- tests/features/step_definitions/payment.spec.js
+++ tests/features/step_definitions/payment.spec.js
@@ -12,4 +12,112 @@ export default function () {
     }, VAL.timeout_out, 'checkout pyament option should exist');
     browser.click(FRONTEND.order.checkout_payment_option);
   });
+
+  this.Then(/^I complete Checkout Frames with a (.*) card$/, (option) => {
+    let card;
+    let mounth;
+    let year;
+    let cvv;
+    browser.waitUntil(function () {
+      return browser.isVisible(FRONTEND.order.checkout_iframe_selector);
+    }, VAL.timeout_out, 'the embedded form should be visible');
+    const iframe = browser.element(FRONTEND.order.checkout_iframe_selector);
+    browser.frame(iframe.value);
+    switch (option) {
+      case 'visa':
+        card = browser.element(FRONTEND.order.embedded_fields.card_number);
+        card.setValue(VAL.card.visa.card_number);
+        mounth = browser.element(FRONTEND.order.embedded_fields.mounth);
+        mounth.setValue(VAL.card.visa.mounth);
+        year = browser.element(FRONTEND.order.embedded_fields.year);
+        year.setValue(VAL.card.visa.year);
+        cvv = browser.element(FRONTEND.order.embedded_fields.cvv);
+        cvv.setValue(VAL.card.visa.cvv);
+        break;
+      case 'mastercard':
+        card = browser.element(FRONTEND.order.embedded_fields.card_number);
+        card.setValue(VAL.card.mastercard.card_number);
+        mounth = browser.element(FRONTEND.order.embedded_fields.mounth);
+        mounth.setValue(VAL.card.mastercard.mounth);
+        year = browser.element(FRONTEND.order.embedded_fields.year);
+        year.setValue(VAL.card.mastercard.year);
+        cvv = browser.element(FRONTEND.order.embedded_fields.cvv);
+        cvv.setValue(VAL.card.mastercard.cvv);
+        break;
+      case 'amex':
+        card = browser.element(FRONTEND.order.embedded_fields.card_number);
+        card.setValue(VAL.card.amex.card_number);
+        mounth = browser.element(FRONTEND.order.embedded_fields.card.mounth);
+        mounth.setValue(VAL.card.amex.mounth);
+        year = browser.element(FRONTEND.order.embedded_fields.card.year);
+        year.setValue(VAL.card.amex.year);
+        cvv = browser.element(FRONTEND.order.embedded_fields.card.cvv);
+        cvv.setValue(VAL.card.amex.cvv);
+        break;
+      case 'diners':
+        card = browser.element(FRONTEND.order.embedded_fields.card_number);
+        card.setValue(VAL.card.diners.card_number);
+        mounth = browser.element(FRONTEND.order.embedded_fields.mounth);
+        mounth.setValue(VAL.card.diners.mounth);
+        year = browser.element(FRONTEND.order.embedded_fields.year);
+        year.setValue(VAL.card.diners.year);
+        cvv = browser.element(FRONTEND.order.embedded_fields.cvv);
+        cvv.setValue(VAL.card.diners.cvv);
+        break;
+      case 'jcb':
+        card = browser.element(FRONTEND.order.embedded_fields.card_number);
+        card.setValue(VAL.card.jcb.card_number);
+        mounth = browser.element(FRONTEND.order.embedded_fields.mounth);
+        mounth.setValue(VAL.card.jcb.mounth);
+        year = browser.element(FRONTEND.order.embedded_fields.year);
+        year.setValue(VAL.card.jcb.year);
+        cvv = browser.element(FRONTEND.order.embedded_fields.cvv);
+        cvv.setValue(VAL.card.jcb.cvv);
+        break;
+      case 'discover':
+        card = browser.element(FRONTEND.order.embedded_fields.card_number);
+        card.setValue(VAL.card.discover.card_number);
+        mounth = browser.element(FRONTEND.order.embedded_fields.mounth);
+        mounth.setValue(VAL.card.discover.mounth);
+        year = browser.element(FRONTEND.order.embedded_fields.year);
+        year.setValue(VAL.card.discover.year);
+        cvv = browser.element(FRONTEND.order.embedded_fields.cvv);
+        cvv.setValue(VAL.card.discover.cvv);
+        break;
+      default:
+        card = browser.element(FRONTEND.order.embedded_fields.card_number);
+        card.setValue(VAL.card.visa.card_number);
+        mounth = browser.element(FRONTEND.order.embedded_fields.mounth);
+        mounth.setValue(VAL.card.visa.mounth);
+        year = browser.element(FRONTEND.order.embedded_fields.year);
+        year.setValue(VAL.card.visa.year);
+        cvv = browser.element(FRONTEND.order.embedded_fields.cvv);
+        cvv.setValue(VAL.card.visa.cvv);
+        break;
+    }
+    browser.frameParent();
+  });
+
+  this.Then(/^I submit the order for the (.*) integration$/, (option) => {
+    browser.pause(1000); // Make sure the card token is generated
+    switch (option) {
+      case 'frames':
+        browser.waitUntil(function () {
+          return browser.isEnabled(FRONTEND.order.place_order_button);
+        }, VAL.timeout_out, 'place order button should be enabled');
+        browser.click(FRONTEND.order.place_order_button);
+        break;
+      case 'hosted':
+        browser.waitUntil(function () {
+          return browser.isEnabled(FRONTEND.order.hosted_place_order);
+        }, VAL.timeout_out, 'place order button should be enabled');
+        browser.click(FRONTEND.order.hosted_place_order);
+        break;
+      default:
+        break;
+    }
+    browser.waitUntil(function () {
+      return browser.isEnabled('asdas');
+    }, VAL.timeout_out, 'should fail');
+  });
 }
