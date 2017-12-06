86e780e7b69fab47354ea81947fd6a435932ce30
Ioan Ghisoi <ioan.ghisoi@checkout.com>
2017-12-06 17:12:14 +0000

    3d secure

--- tests/features/step_definitions/order.spec.js
+++ tests/features/step_definitions/order.spec.js
@@ -54,8 +54,7 @@ export default function () {
         browser.waitUntil(function () {
           return !browser.getAttribute(FRONTEND.order.email_fieldset, 'class').includes(VAL.fieldset_block);
         }, VAL.timeout_out, 'the email check should be completed');
-        console.log(browser.getValue(FRONTEND.order.customer_email) === VAL.guest.email);
-        console.log(browser.getValue(FRONTEND.order.customer_phone) === VAL.guest.phone);
+        browser.pause(2000); // avoid magento problems
         if (browser.getValue(FRONTEND.order.customer_firstname) === VAL.guest.name && browser.getValue(FRONTEND.order.customer_phone) === VAL.guest.phone) {
           browser.waitUntil(function () {
             return !browser.getAttribute('body', 'class').includes(FRONTEND.order.ajax_loader);
